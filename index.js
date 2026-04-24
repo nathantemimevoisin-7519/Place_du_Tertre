59054f138b63e63d52ddcf9066de77856dbd4ecf
Nathan <nathan.temimevoisin@gmail.com>
2026-04-24 17:04:57 +0200

    faire s'afficher les images droites en positions randoms

---
+++ index.js
@@ -0,0 +1,17 @@
+const espace = document.getElementById("espace")
+console.log(espace);
+images = ["abbaye_de_montmartre.jpg", "Adolphe_Thiers_Nadar.jpg", "execution_lecomte.jpg", "Le_siecle.jpg", "Louise_Michel.jpg", "plaque_voiture.jpg"]
+
+function create_piece(image) {
+    const piece = document.createElement("div");
+    piece.classList.add("piece");
+    piece.style.backgroundImage = `url(${image})` //"url(Adolphe_Thiers_Nadar.jpg)";
+    piece.style.left = `${Math.random() * 1000}px`;
+    piece.style.top = `${Math.random() * 250}px`;
+    espace.appendChild(piece);
+    console.log(piece);
+}
+for (img in images) {
+    console.log(images[img])
+    create_piece(images[img])
+}
