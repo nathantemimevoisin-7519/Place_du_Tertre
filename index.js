c5d92d29743eef7c6244f5e387ac46d4852b8c7f
Nathan <nathan.temimevoisin@gmail.com>
2026-04-28 17:49:12 +0200

    creation fonction qd on clique sur image et faire qu'elle se mette à l'avant plan

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const espace = document.getElementById("espace")
 console.log(espace);
 images = ["abbaye_de_montmartre.jpg", "Adolphe_Thiers_Nadar.jpg", "execution_lecomte.jpg", "Le_siecle.jpg", "Louise_Michel.jpg", "plaque_voiture.jpg"]
+let zCompteur = 0
 
 function create_piece(image) {
     const piece = document.createElement("div");
@@ -11,6 +12,14 @@ function create_piece(image) {
     piece.style.rotate = `${Math.random() * 360}deg`;
     espace.appendChild(piece);
     console.log(piece);
+
+    piece.onpointerdown = function (e) {
+        piece.style.zIndex = ++zCompteur
+        piece.style.border = "3px solid green"
+    }
+    piece.onpointerup = function (e) {
+        piece.style.border = "1px solid black"
+    }
 }
 for (img in images) {
     console.log(images[img])
