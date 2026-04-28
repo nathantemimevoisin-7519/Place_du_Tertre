5e62e74f594f09b619e28b4e38ca7e8a856b50fa
Nathan <nathan.temimevoisin@gmail.com>
2026-04-28 18:07:53 +0200

    audio se lance quand on clique sur photo

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const espace = document.getElementById("espace")
 console.log(espace);
 images = ["abbaye_de_montmartre.jpg", "Adolphe_Thiers_Nadar.jpg", "execution_lecomte.jpg", "Le_siecle.jpg", "Louise_Michel.jpg", "plaque_voiture.jpg"]
+audios = ["abbaye royale Montmartre.mp3", "thiers.mp3", "lecomte.mp3", "siecle.mp3", "michel.mp3", "voiture.mp3"]
 let zCompteur = 0
 
-function create_piece(image) {
+function create_piece(num) {
     const piece = document.createElement("div");
     piece.classList.add("piece");
-    piece.style.backgroundImage = `url(${image})` //"url(Adolphe_Thiers_Nadar.jpg)";
+    piece.style.backgroundImage = `url(${images[num]})` //"url(Adolphe_Thiers_Nadar.jpg)";
     piece.style.left = `${Math.random() * 1000}px`;
     piece.style.top = `${Math.random() * 250}px`;
     piece.style.rotate = `${Math.random() * 360}deg`;
@@ -16,12 +17,13 @@ function create_piece(image) {
     piece.onpointerdown = function (e) {
         piece.style.zIndex = ++zCompteur
         piece.style.border = "3px solid green"
+        new Audio(audios[num]).play()
     }
     piece.onpointerup = function (e) {
         piece.style.border = "1px solid black"
     }
 }
-for (img in images) {
-    console.log(images[img])
-    create_piece(images[img])
+for (let i = 0; i < 6; i++) {
+    console.log(images[i])
+    create_piece(i)
 }
