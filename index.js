ca6153699656c197d5faa07a4076ced0a96d1d63
Nathan <nathan.temimevoisin@gmail.com>
2026-05-03 18:42:50 +0200

    ajout audios

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const espace = document.getElementById("espace")
 console.log(espace);
 images = ["abbaye_de_montmartre.jpg", "Adolphe_Thiers_Nadar.jpg", "execution_lecomte.jpg", "Le_siecle.jpg", "Louise_Michel.jpg", "plaque_voiture.jpg", "telegraphe.png", "bouscarat.png"]
-audios = ["abbaye royale Montmartre.mp3", "thiers.mp3", "laura_montage_lecomte.mp3", "siecle.mp3", "michel.mp3", "voiture.mp3", "telegraphe.mp3", "bouscarat.mp3"]
+audios = ["lucie_abbaye.mp3", "thiers.mp3", "laura_montage_lecomte.mp3", "lucie_temoignage_le_siecle.mp3", "vali.mp3", "voiture.mp3", "telegraphe.mp3", "flo_orland.mp3"]
 let zCompteur = 10
 
 function create_piece(num) {
     const piece = document.createElement("div");
     piece.classList.add("piece");
-    piece.style.backgroundImage = `url(${images[num]})` //"url(Adolphe_Thiers_Nadar.jpg)";
+    piece.style.backgroundImage = `url(${images[num]})`
     piece.style.left = `${Math.random() * 1000}px`;
     piece.style.top = `${Math.random() * 250}px`;
     piece.style.rotate = `${Math.random() * 360}deg`;
@@ -16,13 +16,16 @@ function create_piece(num) {
 
     piece.onpointerdown = function (e) {
         piece.style.zIndex = ++zCompteur
-        //piece.style.border = "3px solid green"
         new Audio(audios[num]).play()
+        console.log("deb")
+        for (let i = 0; i < 8; i++) {
+            if (!audio.paused && !audio.ended) {
+                console.log("arret audio");
+                audio.stop()
+            }
+        }
     }
 
-    //piece.onpointerup = function (e) {
-    //piece.style.border = "1px solid black"
-    //}
 }
 for (let i = 0; i < 8; i++) {
     console.log(images[i])
